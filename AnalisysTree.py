d3f01ce7db03ca97926c92592627545adb4ae38f
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-04-12 21:04:20 -0300

    Função funcionando para derivação a esquerda

--- AnalisysTree.py
+++ AnalisysTree.py
@@ -1,23 +1,27 @@
 arv = {
     "<inteiro>":["<digito>","<digito><inteiro>"],
-    "<digito>":["0","1","2","3","4","5","6","7","8","9"]
+    "<digito>": ["4","5","2","0"]
 }
+#["0","1","2","3","4","5","6","7","8","9"]
 import re
-inicio = ["<inteiro>"]
+inicio = "<inteiro>"
 padrao = r'<[a-z]+>'
 goal = ["4","5","2","0"]
-def generatorTree(inicio):
-    for _ in range(len(inicio)):
-        last = inicio[0]
-        try:
-            f = (re.findall(padrao,last))[0]
-            p = arv[f]
-            inicio.pop(0)
-            for v in p:
-                s = last
-                inicio.append(s.replace(f,v,1))
-        except Exception:
-            print(inicio[0])  
-            pass      
-    generatorTree(inicio)
-print(generatorTree(inicio))
+deep_boundary = len(goal)*2
+def generatorTree(inicio,deep):
+    if inicio==''.join(goal):
+        return True
+    if deep >= deep_boundary:
+        return False 
+    try:
+        f = (re.findall(padrao,inicio))[0]
+        p = arv[f]
+        for v in p:
+            s = inicio.replace(f,v,1)
+            b = generatorTree(s,deep+1)
+            if b:
+                print(s)
+                return True
+    except Exception:
+        pass      
+print(generatorTree(inicio,0))
